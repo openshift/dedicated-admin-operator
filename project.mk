4410d39aa38434d47000e1c24ddd502bf8863cb4
Naveen Malik <nmalik@redhat.com>
2019-04-05 08:49:56 -0400

    Added namespace to make env

--- project.mk
+++ project.mk
@@ -3,6 +3,7 @@ IMAGE_REGISTRY?=quay.io
 IMAGE_REPOSITORY?=openshift-sre
 IMAGE_NAME?=dedicated-admin-operator
 OPERATOR_NAME?=$(IMAGE_NAME)
+OPERATOR_NAMESPACE?=openshift-dedicated-admin
 
 VERSION_MAJOR?=0
 VERSION_MINOR?=1
